7ae06f1a3abcda5432cc36d2745a43ab624585af
David Quilter <davidquilter@Davids-MacBook-Pro.local>
2015-08-23 16:48:54 +0100

    Basic musical pattern written.

--- js/audio.js
+++ js/audio.js
@@ -22,41 +22,108 @@ var itsChristmas = {
 
 		for(i = 0; i < itsChristmas.buttons.length; i++) {
 			itsChristmas.buttons[i].onclick = itsChristmas.createDing;
-			itsChristmas.checkPosition();
 		}
 	
 	},
 
 	sequence: {
 		1: [4, 4, 4],
-		2: [4, 4, 4]
+		2: [4, 4, 4],
+		3: [4, 6, 2, 3, 4]
 	},
 	
-	checkPosition: function() {
+	checkPosition: function(note) {
+		var newNote;
+		var newNoteElem;
+		var oldNote;
+		var nextStep;
+		if(itsChristmas.step === 0) {
+			if(itsChristmas.counter < 4) {
+				itsChristmas.counter++;
+			} else {
+				itsChristmas.nextStep(1);
+			}
+		} else {
+			
+			if(note === undefined) {
+				// First note
+				console.log('first')
+				newNote = itsChristmas.sequence[itsChristmas.step][itsChristmas.counter];
+				console.log(newNote);
 
+				newNoteElem = document.querySelector('[data-note="' + newNote + '"]');
+				newNoteElem.classList.add('highlight');
+			} else {
+				// Subsequent notes
+				// Check the right note was played
+				console.log('subsequent');
+				oldNote = itsChristmas.sequence[itsChristmas.step][itsChristmas.counter];
+				console.log(itsChristmas.counter);
+				
+				if(parseInt(note, 10) === parseInt(oldNote, 10)) {
+					if(itsChristmas.counter + 1 !== itsChristmas.sequence[itsChristmas.step].length) {
+						itsChristmas.counter++;
+						newNote = itsChristmas.sequence[itsChristmas.step][itsChristmas.counter];
+						newNoteElem = document.querySelector('[data-note="' + newNote + '"]');
+						newNoteElem.classList.add('highlight');
+					} else {
+						nextStep = parseInt(itsChristmas.step, 10) + 1;
+						console.log(nextStep)
+						console.log(typeof(nextStep))
+						itsChristmas.nextStep(nextStep)
+					}
+				} else {
+					itsChristmas.wrong();
+				}
+			}
+			
+		}
+	},
+	
+	wrong: function() {
+		console.log('Wrong!');
+	},
+	
+	nextStep: function(next) {
+		var oldStep = document.querySelector('[data-message="' + (next - 1) + '"]');
+		var newStep = document.querySelector('[data-message="' + next + '"]');
+
+		itsChristmas.counter = 0;
+		itsChristmas.step++;
+		
+		oldStep.style.display = 'none';
+		newStep.style.display = 'block';
+		
+		console.log('NEXT!');
+		itsChristmas.checkPosition();
+		
 	},
 	
 	createDing: function(evt) {
 		evt.preventDefault();
 		var frequency = this.getAttribute('data-note');
 		console.log('playing');
-
+		
+		var noteElem = document.querySelector('.highlight');
+		if(noteElem !== null) {
+			noteElem.classList.remove('highlight');
+		}
+		
 		// [frequency, type, startTime, fadeMidTime, fadeEndTime]
 		var baseArray = [itsChristmas.frequencyArray[frequency], 'triangle', 0, 0.5, 1];
 		var depthArray = [itsChristmas.frequencyArray[frequency], 'square', 0.2, 1.2, 1.5];
 
 		itsChristmas.createSound(baseArray);
 		itsChristmas.createSound(depthArray);
-
+		
+		itsChristmas.checkPosition(frequency);
 	},
 
 	createSound: function(array) {
 		var context = itsChristmas.context;
-		console.log(array);
 		oscillator = context.createOscillator();
 		gainNode = context.createGain();
 		gainNode.gain.value = 1;
-		console.log(gainNode);
 		oscillator.type = array[1];
 		oscillator.frequency.value = array[0]; // value in hertz
 		oscillator.connect(gainNode);
